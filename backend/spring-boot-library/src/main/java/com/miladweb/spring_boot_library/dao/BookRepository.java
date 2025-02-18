1cdcb253b61777918d9610f5a8021da2376b0c73
miladmo68 <miladmo68@gmail.com>
2025-02-18 14:02:00 -0500

    Initial commit

--- backend/spring-boot-library/src/main/java/com/miladweb/spring_boot_library/dao/BookRepository.java
+++ backend/spring-boot-library/src/main/java/com/miladweb/spring_boot_library/dao/BookRepository.java
@@ -1,7 +1,20 @@
 package com.miladweb.spring_boot_library.dao;
 
 import com.miladweb.spring_boot_library.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+
+    Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
+
+
 }
---
+++ frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -0,0 +1,218 @@
+import { useState, useEffect } from "react";
+import BookModel from "../../models/BookModel";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
+import { SearchBook } from "./components/SearchBook";
+import { Pagination } from "../Utils/Pagination";
+
+export const SearchBooksPage = () => {
+  const [books, setBooks] = useState<BookModel[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [booksPerPage] = useState(5);
+  const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [search, setSearch] = useState("");
+  const [searchUrl, setSearchUrl] = useState("");
+  const [categorySelection, setCategorySelection] = useState("Book category");
+
+  useEffect(() => {
+    const fetchBook = async () => {
+      const baseUrl: string = "http://localhost:8080/api/books";
+
+      let url: string = "";
+
+      if (searchUrl === "") {
+        url = `${baseUrl}?page=${currentPage - 1}&size=${booksPerPage}`;
+      } else {
+        url = baseUrl + searchUrl;
+        let searchWithPage = searchUrl.replace(
+          "<pageNumber>",
+          `${currentPage - 1}`
+        );
+        url = baseUrl + searchWithPage;
+      }
+
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const responseJson = await response.json();
+      const responseData = responseJson._embedded.books;
+
+      setTotalAmountOfBooks(responseJson.page.totalElements);
+      setTotalPages(responseJson.page.totalPages);
+
+      const LoadedBooks: BookModel[] = [];
+      for (const key in responseData) {
+        LoadedBooks.push({
+          id: responseData[key].id,
+          title: responseData[key].title,
+          author: responseData[key].author,
+          description: responseData[key].description,
+          copies: responseData[key].copies,
+          copiesAvailable: responseData[key].copiesAvailable,
+          category: responseData[key].category,
+          img: responseData[key].img,
+        });
+      }
+      setBooks(LoadedBooks);
+      setIsLoading(false);
+    };
+    fetchBook().catch((error: any) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+    window.scrollTo(0, 0);
+  }, [currentPage, searchUrl]);
+
+  if (isLoading) {
+    return <SpinnerLoading />;
+  }
+  if (httpError) {
+    return (
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
+  }
+  const searchHandleChange = () => {
+    setCurrentPage(1);
+    if (search === "") {
+      setSearchUrl("");
+    } else {
+      setSearchUrl(
+        `/search/findByTitleContaining?title=${search}&page=<pageNumber>&size=${booksPerPage}`
+      );
+    }
+    setCategorySelection("Book category");
+  };
+
+  const categoryField = (value: string) => {
+    setCurrentPage(1);
+    if (
+      value.toLowerCase() === "fe" ||
+      value.toLowerCase() === "be" ||
+      value.toLowerCase() === "data" ||
+      value.toLowerCase() === "devops"
+    ) {
+      setCategorySelection(value);
+      setSearchUrl(
+        `/search/findByCategory?category=${value}&page=<pageNumber>&size=${booksPerPage}`
+      );
+    } else {
+      setCategorySelection("All");
+      setSearchUrl(`?page=<pageNumber>&size=${booksPerPage}`);
+    }
+  };
+
+  const indexOfLastBook: number = currentPage * booksPerPage;
+  const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
+  let lastItem =
+    booksPerPage * currentPage <= totalAmountOfBooks
+      ? booksPerPage * currentPage
+      : totalAmountOfBooks;
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  return (
+    <div>
+      <div className="container">
+        <div className="row mt-5">
+          <div className="col-6">
+            <div className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-labelledby="Search"
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <button
+                className="btn btn-outline-success"
+                onClick={() => searchHandleChange()}
+              >
+                Search
+              </button>
+            </div>
+          </div>
+          <div className=" col-4">
+            <div className="dropdown">
+              <button
+                className="btn btn-secondary dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                {categorySelection}
+              </button>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton1"
+              >
+                <li onClick={() => categoryField("All")}>
+                  <a className="dropdown-item" href="#">
+                    All
+                  </a>
+                </li>
+                <li onClick={() => categoryField("FE")}>
+                  <a className="dropdown-item" href="#">
+                    Front End
+                  </a>
+                </li>
+                <li onClick={() => categoryField("BE")}>
+                  <a className="dropdown-item" href="#">
+                    Back En
+                  </a>
+                </li>
+                <li onClick={() => categoryField("Data")}>
+                  <a className="dropdown-item" href="#">
+                    Data
+                  </a>
+                </li>
+                <li onClick={() => categoryField("DevOps")}>
+                  <a className="dropdown-item" href="#">
+                    DevOp
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="mt-3">
+            {totalAmountOfBooks > 0 ? (
+              <>
+                <h5>Number of results: ({totalAmountOfBooks})</h5>
+                <p>
+                  {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks}
+                  items:
+                </p>
+                {books.map((book) => (
+                  <SearchBook book={book} key={book.id} />
+                ))}
+              </>
+            ) : (
+              <div className="m-5">
+                <h3>Can't find what you are looking for?</h3>
+                <a
+                  type="button"
+                  className="btn main-color btn-md px-4 me-md-2 fw-bold text-white"
+                  href="#"
+                >
+                  Library Services
+                </a>
+              </div>
+            )}
+            {totalPages > 1 && (
+              <Pagination
+                currentPage={currentPage}
+                totalPages={totalPages}
+                paginate={paginate}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
